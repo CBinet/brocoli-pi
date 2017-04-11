b95983f35fabf282e0890b650532fc6e10b2869f
Charles <cbinet1111@gmail.com>
2017-04-11 12:54:57 -0400

    auto-commit

--- gitpull.sh
+++ gitpull.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "Hello my Lord! We are pulling your stuff right now, please be patient. :)"
+echo "Hello my Lord! We are pulling your stuff right now, then starting the server. Please be patient. :)"
 git pull
+python server.py
