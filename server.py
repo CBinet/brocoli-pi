92115a0086a947f374a0a77dae0346074bf5132a
Charles <cbinet1111@gmail.com>
2017-04-11 12:48:53 -0400

    auto-commit

--- server.py
+++ server.py
@@ -22,7 +22,7 @@ outputs = [Output(17, "Red Light"),Output(18, "Green Light"),Output(19, "Yellow 
 # -- API Routes -- 
 @app.route('/outputs')
 def getOutputs():
-    results = [ output.as_dict() for output in outputs ]
+    results = [ output.toJSON for output in outputs ]
     return jsonify({count: len(results), results: results})
 
 @app.route('/outputs/<id>')
