573c52259dd53cf9e365ea856cb2660d22f5919e
Charles <cbinet1111@gmail.com>
2017-04-11 02:27:38 -0400

    auto-commit

--- server.py
+++ server.py
@@ -13,10 +13,17 @@ outputs = [Output(19)]
 
 @app.route('/<id>/status')
 def getStatus(id):
+    output = findOutput(id)
+    if output:
+        return jsonify(result = output.getCurrentState())
+    else:
+        return "NOT_FOUND : This output ID is not binded."
+
+
+def findOutput(id):
     for output in outputs:
 	if id == str(output.getId()):
-	    return jsonify(result = output.getCurrentState())
-    return "NOT_FOUND : This output ID is not binded."
+	    return output
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
