0de7779f571815d30aa9ca805bb76c17327995d4
Charles <cbinet1111@gmail.com>
2017-04-10 22:18:28 -0400

    oui

--- script.py
+++ script.py
@@ -1,3 +1,17 @@
+import RPi.GPIO as GPIO
+import time
 import datetime
-print (str(datetime.datetime.now().time()))
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(19, GPIO.OUT)
+
+x = 0
+while x < 5
+    GPIO.output(19,True)
+    time.sleep(1)
+    GPIO.output(19,False)
+    x = x + 1
+
+print ("Time : " + str(datetime.datetime.now().time()))
 
