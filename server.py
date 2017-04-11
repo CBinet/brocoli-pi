113dc506f758e1822e498d9783a8c3d0828b42eb
Charles <cbinet1111@gmail.com>
2017-04-11 13:17:39 -0400

    auto-commit

--- server.py
+++ server.py
@@ -27,7 +27,7 @@ def getOutputs():
     for output in outputs:
         rtrn.append(output.toJSON())
     
-    return jsonify({'results' : ['a', 'b', 'c']})
+    return {'results' : rtrn}
 
 @app.route('/outputs/<id>')
 def getOutput(id):
