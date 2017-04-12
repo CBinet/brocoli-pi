2804dda75e413ec18c358d4bce447b1784cfff5a
Charles <cbinet1111@gmail.com>
2017-04-12 01:54:43 -0400

    auto-commit

--- server.py
+++ server.py
@@ -102,12 +102,7 @@ def toggleGroup(id):
 @app.route('/')
 def get_data():
     r = requests.get('https://owapi.net/api/v3/u/Viiarge-1583/blob');
-
-    return Response(
-        r.text
-        status=r.status_code,
-        content_type=r.headers['content-type'],
-    )
+    return make_response(json.dumps(r.json(), indent=4), 200)
 
 # -- Helper functions --
 
